eb0276c3e2b6fe5f937a3aea234e52747133c2d6
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-23 18:05:41 +0330

    create method get

--- base/run.py
+++ base/run.py
@@ -4,5 +4,6 @@ from apps.company.models import Company
 
 if __name__ == '__main__':
     Company.objects.create_table()
-    res = Company.objects.get(1)
+    # res = Company.objects.get(id=1)
+    print(Company.objects.get(name='amir'))
     app.run()
