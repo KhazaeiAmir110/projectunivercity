0594ca9bab9d140f50b640def31b85cee81702be
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-24 17:08:05 +0330

    add static files

--- apps/company/routes.py
+++ apps/company/routes.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blueprint = Blueprint('company', __name__)
+
+
+@blueprint.route('/')
+def home():
+    return render_template('company/page1.html')
