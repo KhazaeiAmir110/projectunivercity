de5c01c2bd602ae4f5345b3c66d264e88ce5d341
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-24 08:19:21 +0330

    create method create

--- base/run.py
+++ base/run.py
@@ -5,5 +5,6 @@ from apps.company.models import Company
 if __name__ == '__main__':
     Company.objects.create_table()
     # res = Company.objects.get(id=1)
-    print(Company.objects.delete(name='amir'))
+    # print(Company.objects.delete(name='amir'))
+    print(Company.objects.create(name='wsfd', description='asdf', address='sdfsdf', slug='sdfsdf', user_id=4))
     app.run()
