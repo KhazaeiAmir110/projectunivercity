981e3bb543944bd3c59ecae3954fba6fa546e8c2
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-24 08:44:28 +0330

    add model user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,1 +1,22 @@
-from base.secret import db
+from apps.core.database import Database
+from apps.core.orm import ORMMixin
+
+
+class UserManager(ORMMixin, Database):
+    _create_table_query = """
+        CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            password TEXT NOT NULL,
+            is_superuser BOOLEAN NOT NULL,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            is_active BOOLEAN NOT NULL,
+            date_joined TIMESTAMP NOT NULL,
+            username TEXT NOT NULL UNIQUE,
+            phone TEXT NOT NULL UNIQUE,
+            otp TEXT
+        );
+    """
+
+class User:
+    objects = UserManager()
