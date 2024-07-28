73a763b3ecd275acd7341414f554e384ff1dd8bd
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-28 07:09:21 +0330

    update page 1

--- apps/company/routes.py
+++ apps/company/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from apps.company.models import Company
+
 blueprint = Blueprint('company', __name__)
 
 
 @blueprint.route('/')
 def home():
-    return render_template('company/page1.html')
+
+    return render_template('company/page1.html', companies=Company.objects.filter(is_active=1))
