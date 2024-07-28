1a39080127ba76e7379935c266afdb45372acc50
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-28 06:16:49 +0330

    fix bug

--- base/create_table.py
+++ base/create_table.py
@@ -1,10 +1,9 @@
+from apps.users.models import User
 from apps.company.models import (
     Company, HolidaysDate, SansConfig, Reservation)
-from apps.users.models import User
 
+User.objects.create_table()
 Company.objects.create_table()
 HolidaysDate.objects.create_table()
 SansConfig.objects.create_table()
 Reservation.objects.create_table()
-User.objects.create_table()
-
