e4728c5ef3a36bb1657b96a232e2bb502c907f69
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-29 09:43:23 +0330

    create table sansholidaydatetime

--- base/create_table.py
+++ base/create_table.py
@@ -1,9 +1,10 @@
 from apps.users.models import User
 from apps.company.models import (
-    Company, HolidaysDate, SansConfig, Reservation)
+    Company, HolidaysDate, SansConfig, SansHistoryDate, Reservation)
 
 User.objects.create_table()
 Company.objects.create_table()
 HolidaysDate.objects.create_table()
 SansConfig.objects.create_table()
+SansHistoryDate.objects.create_table()
 Reservation.objects.create_table()
