e62dda1e615f3df2ea17556c7adaff42b400b426
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-29 09:04:09 +0330

    update function home

--- apps/company/routes.py
+++ apps/company/routes.py
@@ -1,11 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 from apps.company.models import Company
+from apps.users.models import User
 
 blueprint = Blueprint('company', __name__)
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=("GET", "POST"))
 def home():
+    if request.method == "POST":
+        company_slug = request.form.get('company_slug')
+        if company_slug:
+            return redirect('company.company_detail', company_slug)
 
-    return render_template('company/page1.html', companies=Company.objects.filter(is_active=1))
+    return render_template('company/page1.html',
+                           companies=Company.objects.filter(is_active=1),
+                           join_table=Company.objects.left_join(join_table=User,
+                                                                join_condition='company.user_id=user.id')
+                           )
