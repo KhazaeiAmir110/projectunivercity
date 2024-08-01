214b08a0c32e8ac43a8be3381f40bf84428dbc40
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-01 09:18:27 +0330

    update config

--- base/config.py
+++ base/config.py
@@ -1,11 +1,13 @@
 import os
 
+import secret
+
 
 class Config(object):
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     CSRF_ENABLED = True
-    CSRF_SESSION_KEY = 'CSRF_SESSION_KEY'
-    SECRET_KEY = 'SECRET_KEY'
+    CSRF_SESSION_KEY = secret.CSRF_SESSION_KEY
+    SECRET_KEY = secret.SECRET_KEY
 
 
 class DevelopmentConfig(Config):
